f65bb8b2956ac908b7ec576e73a57bd6f261565f
Odette <uweraodette01@gmail.com>
2026-01-19 10:00:18 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <h1>The Frontend, React</h1>
+      <p>This is the React frontend application.</p>
+    </>
+  );
+};
+
+export default App
